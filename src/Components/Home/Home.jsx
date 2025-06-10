eededac045726bafa5c824c1a3bfe3a1b6a6263c
pushkardubey123 <pushkardubey1407@gmail.com>
2025-06-10 17:00:48 +0530

    first commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,6 +4,9 @@ import Banner from './Banner'
 import Footer from './Footer'
 
 const Home = () => {
+  useEffect(() => {
+  fetch("https://backend-hrms-k73a.onrender.com/");
+}, []);
   return (
     <>
    <Banner/>
